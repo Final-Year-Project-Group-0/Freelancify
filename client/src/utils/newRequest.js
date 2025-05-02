74d805a6d7a995efb74ccccfc49b1edc09586168
Final-Year-Project-Group-0 <final.year.project.group.0@gmail.com>
2025-05-03 00:32:54 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://freelancify-pzti.onrender.com/api/",
+  baseURL: "https://freelancify-pzti.onrender.com/api",
   withCredentials: true,
 });
 
